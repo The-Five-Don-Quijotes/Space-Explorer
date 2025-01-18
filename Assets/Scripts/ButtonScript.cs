2f386263a120a576c215398734f0f82f6aa89055
BobJohn69 <minhtuan030304@gmail.com>
2025-01-18 08:59:09 +0700

    Add menu scene

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("Start game");
+        SceneManager.LoadScene("Ingame Scence");
+    }
+
+    public void ShowInstruction()
+    {
+        Debug.Log("Instruction");
+        SceneManager.LoadScene("Instruction Scene");
+    }
+}
